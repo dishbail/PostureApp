5b399b48a83d1facb11080ebe86a17482500c39e
Rebecca <rebeccagolm32@gmail.com>
2021-03-04 16:48:45 -0500

    updated admin page

--- Customer/admin.py
+++ Customer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(PostureRecord)
+admin.site.register(SittingRecord)
+
 
 # Register your models here.
