686c20614ba914f08106410971284362bd3d0ab2
n-saud <56765611+n-saud@users.noreply.github.com>
2021-04-17 21:22:03 -0400

    Add files via upload

--- SensorCode/readdata.py
+++ SensorCode/readdata.py
@@ -2,6 +2,8 @@ import serial
 
 def readdata():
     ser = serial.Serial('COM5',9600); #Connect to the arduino ouput
-    x = ser.readline().decode('UTF-8')
+    x = list(map(int,ser.readline().decode('UTF-8').split(", ")))
     ser.close()
+    if len(x) <256:
+        x = readdata()
     return x
